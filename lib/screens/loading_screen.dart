2440f8fc1f60ac899659fff5d07e008887cce682
gosteve <nomuojamaro@gmail.com>
2020-05-21 16:28:42 +0100

    used the http package to send a http request to open weather api at api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key} using my api key then get back a response body parse te response for the temperature ,city name, and condition code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = '87c6264cd1917ad7cad0b9bbb97ba0ff';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,11 +11,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   getLocation() async {
     //Get the current location
     Location location = Location();
     await location.getCurrentLocation();
-    print('latitude : ${location.latitude} longitude : ${location.longitude}');
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    print('latitude : $latitude longitude : $longitude');
+    getData();
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var temperature = jsonDecode(data)['main']['temp'];
+      var condition = jsonDecode(data)['weather'][0]['id'];
+      var cityName = jsonDecode(data)['name'];
+
+      print(temperature);
+      print(cityName);
+      print(condition);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
